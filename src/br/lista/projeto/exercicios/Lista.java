d63f7464371fb1b44cfd0b82447e09f21982a3c6
devCarlosALexandre <carloalexandresousa@gmail.com>
2023-03-02 21:57:18 -0300

    interando com listas

---
+++ src/br/lista/projeto/exercicios/Lista.java
@@ -0,0 +1,22 @@
+package br.lista.projeto.exercicios;
+
+import br.lista.projeto.implementacao.ListaImpl;
+
+public class Lista {
+    public static void main(String[] args) {
+        ListaImpl lista = new ListaImpl();
+
+        lista.setElement(20);
+        lista.setElement(8);
+        lista.delet(8);
+
+        System.out.println( "A lista está vazia? " +lista.isEmpty());
+        System.out.println( "A lista está cheia? " +lista.isFull());
+        System.out.println(lista.toStringList());
+
+    }
+
+
+
+
+}
---
+++ src/br/lista/projeto/implementacao/ListaImpl.java
@@ -0,0 +1,55 @@
+package br.lista.projeto.implementacao;
+
+public class ListaImpl {
+    private Integer [] elementos = new Integer[10];
+    public int quantiadeElementos(){
+        int quantidade =0;
+        for (int i =0; i < elementos.length; i++){
+            if(elementos[i] != null) {
+                quantidade++;
+            }
+        }
+        return quantidade;
+    };
+
+    public boolean isFull(){
+        return quantiadeElementos() == elementos.length;
+    }
+
+    public boolean isEmpty(){
+        return  quantiadeElementos() ==0;
+    }
+
+    public void setElement(int element){
+        if (isFull()){
+            return;
+        }
+
+        for(int i =0; i < elementos.length; i++){
+            if(elementos[i] == null){
+                elementos[i] = element;
+                break;
+            }
+        }
+
+    }
+
+    public String toStringList(){
+        String listString="";
+        for(int i=0; i< elementos.length; i++){
+           listString +=  elementos[i]+" ";
+        }
+        return listString;
+    }
+
+    public void delet(int element){
+        for(int i =0; i < elementos.length; i++){
+            if(element == elementos[i]){
+                elementos[i] = null;
+                break;
+            }else{
+                break;
+            }
+        }
+    }
+}
